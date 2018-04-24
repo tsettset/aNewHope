dc9d10016dd1f561d2a8212028c9dbddecaf92eb
tsettset <tsettset56@gmail.com>
2018-04-24 17:07:13 +0200

    ajout des fichiers headers nav et footers

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer style="background-color : black; height : 50px;">
+
+ <div class="row">
+  <div class="col-md-5 col-md-offset-2" style="color : white;">
+   <br>
+    Projet backend ifocop Devoo17 par Hat&Fat - publie sous licence GNU/GPL 3 <a href="http://www.gnu.org/licenses/gpl-3.0.html"  class="glyphicon glyphicon-share-alt"></a>
+  </div>
+  <div class="col-md-4 col-md-offset-1" style="color : white;">
+   <br>
+    Le github du site TROC <a href="https://github.com/tsettset/TROC" class="glyphicon glyphicon-share-alt"></a>
+  </div>
+  </div>
+ 
+</footer>
+
+</body>
+</html>
---
+++ header.inc.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+  
+  
+<?php echo '<title>';
+if (isset($GLOBALS['currentPageName']))
+{
+  echo $GLOBALS['currentPageName'].' -  ';
+}
+echo SITENAME .'</title>';
+?>
+
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+<!-- Latest compiled and minified JavaScript -->
+
+
+        
+  <!----------------------------------------------------MODAL CONNEXION EN STANDBY-------------------------------------------------------------------
+    
+    
+     ------------------------------------PARTIE A INSERER DANS LE MENU DEROULANT-----------------------------
+        <li type="button" data-toggle="modal" data-target="#modalSignInForm"><a href="">Connexion</a></li> 
+     --------------------------------------------------------------------------------------------------------
+     
+     
+      <div class="modal fade" id="modalSignInForm" tabindex="-1" role="form" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header text-center">
+              <h4 class="modal-title w-100 font-weight-bold">Sign in</h4>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+
+              <p><?php //include'connexion_controller.php'; ?></p>
+
+
+            </div>
+          </div>
+        </div>
+      </div>              
+    </div>-->
+
+</head>
+<body>
+
+
